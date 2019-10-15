a2b5635832b48ace1e471542d06b8591162d2af6
James Chapman <chapmanjamesg@gmail.com>
2019-10-15 10:35:07 -0500

    fixed the forever loop

---
+++ src/scripts/main.js
@@ -0,0 +1,10 @@
+console.log('this is fucking working because i said so')
+
+
+const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+let y = 1
+
+for (let x = 1; x < cookies.length; x++) {
+    const currentCookie = cookies[x]
+    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+}
